03c5e377bc41a093ad58dccc709d66a2291e2f4b
Gauri Joshi <gauri728@gmail.com>
2022-04-21 13:21:31 -0700

    Update final practice

--- final_practice.rb
+++ final_practice.rb
@@ -1,52 +1,55 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
 
-.greeting
+def greeting
+  puts "Hello!"
+end
+
+greeting
 
 # What is the return value of your method?
-#"Hello!"
+#nil
 # How many arguments did you pass your method?
 #None
 
-
-
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+def custom_greeting
+  puts "Hello, Gauri!"
+end
 
-name = "Gauri"
-name .custom_greeting("Hello",name)
+custom_greeting
 
 # What is the return value of your method?
-#"Hello, Gauri"
+#nil
 # How many arguments did you pass your method?
-#2
+#none
 # What data type was your argument(s)?
 #String
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
-num = 8
-num .square(num)
+def square(num)
+  num * num
+end
+
+square(8)
 
 
 # What is the return value of your method?
-#64
+# => 64
 # How many arguments did you pass your method?
 #1
 # What data type was your argument(s)?
 #integer
 
 
-
 #4: Write a method named full name that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
-first = "Mary"
-middle = "Tyler"
-last = "Moore"
-.fullname(first, middle, last)
-
-
+def full_name(first, middle, last)
+  puts "#{first}, #{middle}, #{last}"
+end
 
 # What is the return value of your method?
-#Mary Tyler Moore
+#nil
 # How many arguments did you pass your method?
 # 3
 # What data type was your argument(s)?
